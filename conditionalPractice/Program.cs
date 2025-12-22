de5e7d9b0eb201a25234d1b5531b11d5d8daf015
Gaurav822131 <gaurav822131@gmail.com>
2025-12-23 00:02:24 +0530

    Added conditional practice C# programs

---
+++ conditionalPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// using System;
+// namespace practiceAssignment;
+// class Program()
+// {
+//     static void Main()
+//     {
+//         HeightCalc.height();
+//     }
+// }
+
+
+
+// using System;
+// namespace practiceAssignment;
+// class Program
+// {
+//     static void Main()
+//     {
+//         LargestNumber.Largest();
+//     }
+// }
+
+
+using System;
+namespace practiceAssignment;
+class Program
+{
+    static void Main()
+    {
+            LeapCheck.Leap();
+    }
+}
